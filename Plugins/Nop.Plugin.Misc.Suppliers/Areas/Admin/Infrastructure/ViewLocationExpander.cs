9f66a3a1e7f6531d30673cc3b113852f2eb9f0ab
tanzin-ahammad-bs <tanzin.ahammad@brainstation-23.com>
2025-04-24 11:18:10 +0600

    View location expander added

---
+++ Plugins/Nop.Plugin.Misc.Suppliers/Areas/Admin/Infrastructure/ViewLocationExpander.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+namespace Nop.Plugin.Misc.Suppliers.Areas.Admin.Infrastructure;
+public class ViewLocationExpander : IViewLocationExpander
+{
+    
+    public void PopulateValues(ViewLocationExpanderContext context)
+    {
+       
+    }
+    public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+    {
+        if (context.AreaName == "Admin")
+        {
+            viewLocations = new string[]
+            {
+                $"/Plugins/Nop.Plugin.Misc.Suppliers/Areas/Admin/Views/{{0}}.cshtml",
+                $"/Plugins/Nop.Plugin.Misc.Suppliers/Areas/Admin/Views/{{1}}/{{0}}.cshtml"
+            }.Concat(viewLocations);
+        }
+        else
+        {
+            viewLocations = new string[]
+            {
+                $"/Plugins/Nop.Plugin.Misc.Suppliers/Areas/Admin/Views/{{0}}.cshtml",
+                $"/Plugins/Nop.Plugin.Misc.Suppliers/Areas/Admin/Views/{{1}}/{{0}}.cshtml"
+            }.Concat(viewLocations);
+        }
+        return viewLocations;
+    }
+}
---
+++ Presentation/Nop.Web/Plugins/Misc.Suppliers/Areas/Admin/Infrastructure/ViewLocationExpander.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using Nop.Core.Infrastructure;
+using Nop.Web.Framework.Themes;
+namespace Nop.Plugin.Misc.Suppliers.Areas.Admin.Infrastructure;
+public class ViewLocationExpander : IViewLocationExpander
+{
+    
+    public void PopulateValues(ViewLocationExpanderContext context)
+    {
+       
+    }
+    public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+    {
+        if (context.AreaName == "Admin")
+        {
+            viewLocations = new string[]
+            {
+                $"/Plugins/Nop.Plugin.Misc.Suppliers/Areas/Admin/Views/{{0}}.cshtml",
+                $"/Plugins/Nop.Plugin.Misc.Suppliers/Areas/Admin/Views/{{1}}/{{0}}.cshtml"
+            }.Concat(viewLocations);
+        }
+        else
+        {
+            viewLocations = new string[]
+            {
+                $"/Plugins/Nop.Plugin.Misc.Suppliers/Areas/Admin/Views/{{0}}.cshtml",
+                $"/Plugins/Nop.Plugin.Misc.Suppliers/Areas/Admin/Views/{{1}}/{{0}}.cshtml"
+            }.Concat(viewLocations);
+        }
+        return viewLocations;
+    }
+}
